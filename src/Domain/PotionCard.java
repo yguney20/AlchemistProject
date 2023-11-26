affe881bd976b8f298412a17cc316804cc49230a
90538 <90538@172.20.144.28>
2023-11-26 17:36:56 +0300

    ata

---
+++ src/Domain/PotionCard.java
@@ -0,0 +1,70 @@
+package Domain;
+import java.util.List;
+
+public class PotionCard {
+	
+
+	
+
+	
+		 	private String potionName;
+		    private String potionType;
+		    private List<IngredientCard> ingredients;
+		    private int points;
+		    private String description;
+		    
+		    public PotionCard(String potionName, String potionType, List<IngredientCard> ingredients, int points, String description) {
+		        this.potionName = potionName;
+		        this.potionType = potionType;
+		        this.ingredients = ingredients;
+		        this.points = points;
+		        this.description = description;
+		    }
+		    
+		    
+		    public String getPotionName() {
+		        return potionName;
+		    }
+
+		    public void setPotionName(String potionName) {
+		        this.potionName = potionName;
+		    }
+
+		    public String getPotionType() {
+		        return potionType;
+		    }
+
+		    public void setPotionType(String potionType) {
+		        this.potionType = potionType;
+		    }
+
+		    public List<IngredientCard> getIngredients() {
+		        return ingredients;
+		    }
+
+		    public void setIngredients(List<IngredientCard> ingredients) {
+		        this.ingredients = ingredients;
+		    }
+
+		    public int getPoints() {
+		        return points;
+		    }
+
+		    public void setPoints(int points) {
+		        this.points = points;
+		    }
+
+		    public String getDescription() {
+		        return description;
+		    }
+
+		    public void setDescription(String description) {
+		        this.description = description;
+		    }
+	}
+
+
+
+
+
+
